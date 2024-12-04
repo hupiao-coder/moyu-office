70e0c5cf8235f2a01e423ffb4e5c2a8b623499f8
Louis <louis.liu.oneself@hotmail.com>
2024-12-04 15:02:02 +0800

    新增 ChakraUI 框架

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import * as React from "react";
+// NextUI - https://nextui.org
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+// ChakraUI - https://www.chakra-ui.com
+import { Provider as ChakraUIProvider } from "@/components/ui/provider"
+
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -16,7 +20,11 @@ export function Providers({ children, themeProps }: ProvidersProps) {
 
   return (
     <NextUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      <NextThemesProvider {...themeProps}>
+        <ChakraUIProvider>
+          {children}
+        </ChakraUIProvider>
+      </NextThemesProvider>
     </NextUIProvider>
   );
 }
