700c18a47e6d71eb6417d407a1b0a39771b65c3a
Louis <louis.liu.oneself@hotmail.com>
2024-12-12 15:57:12 +0800

    设置每日一句的日期为英语

--- app/daily/page.tsx
+++ app/daily/page.tsx
@@ -4,6 +4,7 @@ import { Image } from "@nextui-org/image";
 import { getDailySentence } from "./data";
 import { Sentence } from "./definitions";
 import dayjs from 'dayjs';
+require('dayjs/locale/en');
 import ShareModal from './components/ShareModal';
 
 // 设置页面缓存时间为1小时
@@ -19,7 +20,7 @@ export async function generateMetadata(
 	const previousImages = (await parent).openGraph?.images || []
 	return {
 		title: `${sentence.note} | 每日一句`,
-		description: `${sentence.content}【${sentence.note}】—— ${dayjs(sentence.dateline).format('D MMM, YYYY')}`,
+		description: `${sentence.content}\n${sentence.note}\n—— ${dayjs(sentence.dateline).locale('en').format('D MMM, YYYY')}`,
 		openGraph: {
 			images: [sentence.fenxiang_img, ...previousImages],
 		}
@@ -36,7 +37,7 @@ export default async function DailyPage() {
 			<Card isFooterBlurred fullWidth className="h-screen lg:h-full">
 				<CardHeader className="absolute z-10 flex justify-between items-start bg-black/15 gap-4">
 					<div>
-						<p className="text-white lg:text-xl">{ dayjs(sentence.dateline).format('D MMM, YYYY') }</p>
+						<p className="text-white lg:text-xl">{ dayjs(sentence.dateline).locale('en').format('D MMM, YYYY') }</p>
 						<h4 className="text-white font-medium lg:text-2xl mt-2">{ sentence.content }</h4>
 						<h4 className="text-white font-medium lg:text-2xl mt-2">{ sentence.note }</h4>
 					</div>
