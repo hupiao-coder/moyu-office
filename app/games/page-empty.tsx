9c6dd1f74892095e0bcfab2517a376098199714a
Louis <louis.liu.oneself@hotmail.com>
2024-12-14 20:43:49 +0800

    发布【游戏中心】

---
+++ app/games/page-empty.tsx
@@ -0,0 +1,7 @@
+export default function GamesPage() {
+	return (
+		<main className="container mx-auto px-4 pb-10">
+			<h1 className="flex justify-center items-center text-center gap-1 text-4xl font-bold mb-6">游戏中心即将上线</h1>
+      	</main>
+	);
+}
--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,7 +1,22 @@
+import GameCard from "./components/game-card";
+import GameCreateCard from "./components/game-create-card";
+import { getGames } from "./data/data";
+import { Game } from "./data/definitions";
+
+
 export default function GamesPage() {
+
+	// 获取游戏数据
+	const games: Game[] = getGames();
+
 	return (
-		<main className="container mx-auto px-4 pb-10">
-			<h1 className="flex justify-center items-center text-center gap-1 text-4xl font-bold mb-6">游戏中心即将上线</h1>
-      	</main>
+		<>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+				{ games.map((game: Game, index: number) => (
+					<GameCard key={`game-${index}`} game={game} />
+				))}
+				<GameCreateCard />
+			</div>
+      	</>
 	);
 }
